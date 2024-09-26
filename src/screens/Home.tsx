5415226bbf4d803bb9efe1369476f755bde24bb5
seanolero <seanolero@outlook.com>
2024-09-26 03:31:06 +0300

    Suppress TypeScript error for QrScanner import

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 import QrScanner from 'react-qr-scanner';
 import {Button} from "antd";
 import {MoreOutlined} from "@ant-design/icons";
